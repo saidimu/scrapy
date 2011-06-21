a64e9d04067326bdde0938fd2ec0e6a235fec1f1
saidimu apale <saidimu@gmail.com>
2011-06-20 23:13:21 -0400

    - Extremely hacky, but properly populates the non-empty fields in an Item instance with the values from the corresponding fields of the Django model (derived from django_model in the Item declaration).
    Signed-off-by: saidimu apale <saidimu@gmail.com>

--- scrapy/contrib_exp/djangoitem.py
+++ scrapy/contrib_exp/djangoitem.py
@@ -15,9 +15,9 @@ class DjangoItemMeta(ItemMeta):
                     if model_field.name not in cls.fields:
                         cls.fields[model_field.name] = Field()
                     cls._model_fields.append(model_field.name)
-                    import pdb; pdb.set_trace()
-                    cls[model_field.name] = model_field.value_from_object[cls]  ## copy Django model instance values into DjangoItem
-
+#                    import pdb; pdb.set_trace()
+#                    cls._values[model_field.name] = model_field.value_from_object[cls]  ## copy Django model instance values into DjangoItem
+#        import pdb; pdb.set_trace()
         return cls
 
 
@@ -26,10 +26,39 @@ class DjangoItem(Item):
     __metaclass__ = DjangoItemMeta
 
     django_model = None
+    django_instance = None
+    
+#    def save(self, commit=True):
+#        modelargs = dict((f, self.get(f, None)) for f in self._model_fields)
+#        import pdb; pdb.set_trace()
+#        model = self.django_model(**modelargs)
+#        if commit:
+#            model.save()
+#        return model
+    def populate_fields(self):
+        if self.django_instance:
+            return
+        
+#        modelargs = dict((f, self.get(f, None)) for f in self._model_fields)
+        modelargs = {}
+        for f in self._model_fields:
+            if self.get(f, None):
+                modelargs[f] = self.get(f, None)
+                              
+#        self.django_instance = self.django_model(**modelargs)
+#        self.django_instance = self.django_model.objects.get(**modelargs)   ## assuming only 1 result
+        self._values['django_instance'] = self.django_model.objects.get(**modelargs)   ## assuming only 1 result
+        
+        ## now populate empty fields in this Item with values from the just-retrieved django instance
+        for f in self._model_fields:
+            if not self.get(f, None):
+                self[f] = getattr(self['django_instance'], f)
+        import pdb; pdb.set_trace()
+        
+        
+    def save_to_db(self):
+        if not self.django_instance:
+            self.populate_item()
 
-    def save(self, commit=True):
-        modelargs = dict((f, self.get(f, None)) for f in self._model_fields)
-        model = self.django_model(**modelargs)
-        if commit:
-            model.save()
-        return model
+        self.django_instance = self.django_instance.save()
+        return self.django_instance
